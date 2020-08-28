916059c493470b086e200604a9080a295b1655fc
Makhdoom Shabir <MShabir@qa.com>
2020-08-28 11:18:49 +0100

    changed pom file

---
+++ src/main/java/com/qa/fundamentalproject/FundamentalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.fundamentalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentalprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qa/fundamentalproject/FundamentalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.fundamentalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
