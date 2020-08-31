f43c215f1f5df446b560d095846e4ac9efbfcfad
Makhdoom Shabir <MShabir@qa.com>
2020-08-31 22:33:03 +0100

    created BlogRepository

---
+++ src/main/java/com/qa/fundamentalproject/repo/BlogRepository.java
@@ -0,0 +1,9 @@
+package com.qa.fundamentalproject.repo;
+
+import com.qa.fundamentalproject.domain.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Long> {
+}
