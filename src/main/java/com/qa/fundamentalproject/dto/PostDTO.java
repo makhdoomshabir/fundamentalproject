a47f6fbfafcaf20eb109ddde41e32ef5b5bd18bd
Makhdoom Shabir <MShabir@qa.com>
2020-09-02 15:57:56 +0100

    PostDTO written

--- src/main/java/com/qa/fundamentalproject/dto/PostDTO.java
+++ src/main/java/com/qa/fundamentalproject/dto/PostDTO.java
@@ -2,4 +2,40 @@ package com.qa.fundamentalproject.dto;
 
 public class PostDTO {
 
+    private Long id;
+    private String title;
+    private String body;
+
+    public PostDTO() {
+    }
+
+    public PostDTO(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
 }
