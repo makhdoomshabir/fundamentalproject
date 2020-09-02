de515bf1ae6ba28de600fa0b46419e24237b8335
Makhdoom Shabir <MShabir@qa.com>
2020-09-02 22:02:03 +0100

    updated post controller to return DTOs

--- src/main/java/com/qa/fundamentalproject/rest/PostsController.java
+++ src/main/java/com/qa/fundamentalproject/rest/PostsController.java
@@ -2,8 +2,11 @@ package com.qa.fundamentalproject.rest;
 
 
 import com.qa.fundamentalproject.domain.Post;
+import com.qa.fundamentalproject.dto.PostDTO;
 import com.qa.fundamentalproject.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,25 +22,30 @@ public class PostsController {
     }
 
     @GetMapping("/")
-    public List<Post> getAllPosts(){
-        return this.postService.readAllPosts();
+    public ResponseEntity<List<PostDTO>> getAllPosts(){
+        return ResponseEntity.ok(this.postService.readAllPosts());
     }
+
     @PostMapping("/createPost")
-    public Post createPost(@RequestBody Post post){
-        return this.postService.createPost(post);
+    public ResponseEntity<PostDTO> createPost(@RequestBody Post post){
+        return new ResponseEntity<PostDTO>(this.postService.createPost(post), HttpStatus.CREATED);
     }
+
     @DeleteMapping("/delete/{id}")
-    public Boolean deletePost(@PathVariable Long id){
-        return this.postService.deletePostById(id);
+    public ResponseEntity<?> deletePost(@PathVariable Long id){
+        return this.postService.deletePostById(id)
+                ? ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+                : ResponseEntity.noContent().build();
     }
+
     @GetMapping("/getNoteById/{id}")
-    public Post getPostById(@PathVariable Long id){
-        return this.postService.findPostById(id);
+    public ResponseEntity<PostDTO> getPostById(@PathVariable Long id){
+        return ResponseEntity.ok(this.postService.findPostById(id));
     }
 
     @PutMapping("/updatePost/{id}")
-    public Post updatePost(@PathVariable Long id, @RequestBody Post post){
-        return this.postService.updatePost(id, post);
+    public ResponseEntity<PostDTO> updatePost(@PathVariable Long id, @RequestBody Post post){
+        return ResponseEntity.ok(this.postService.updatePost(id, post));
     }
 
 }
