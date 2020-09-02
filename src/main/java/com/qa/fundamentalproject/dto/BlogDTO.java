17b6f6689ed39c5356079a131721d238540d95bf
Makhdoom Shabir <MShabir@qa.com>
2020-09-02 15:30:39 +0100

    blog DTO written

--- src/main/java/com/qa/fundamentalproject/dto/BlogDTO.java
+++ src/main/java/com/qa/fundamentalproject/dto/BlogDTO.java
@@ -1,4 +1,46 @@
 package com.qa.fundamentalproject.dto;
 
+import com.qa.fundamentalproject.domain.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BlogDTO {
+
+    private Long id;
+    private String name;
+    private List<PostDTO> posts = new ArrayList<>();
+
+    public BlogDTO() {
+    }
+
+    public BlogDTO(String name) {
+        super();
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<PostDTO> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<PostDTO> posts) {
+        this.posts = posts;
+    }
+
 }
--- src/main/java/com/qa/fundamentalproject/dto/PostDTO.java
+++ src/main/java/com/qa/fundamentalproject/dto/PostDTO.java
@@ -1,4 +1,5 @@
 package com.qa.fundamentalproject.dto;
 
 public class PostDTO {
+
 }
