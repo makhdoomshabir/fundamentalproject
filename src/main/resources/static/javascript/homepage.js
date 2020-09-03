77c3d4a74c01f325efb985eb6daf998710003774
Makhdoom Shabir <MShabir@qa.com>
2020-09-03 11:30:17 +0100

    added form to PostProduction.html added how to submit the form in javascript file

--- src/main/resources/static/javascript/homepage.js
+++ src/main/resources/static/javascript/homepage.js
@@ -47,3 +47,23 @@ function displayBlogs(){
     req.open("GET", "http://localhost:8080/getAllBlogs");
     req.send();
 }
+function submitPost(){
+    let elements = document.getElementById("postsForm").elements;
+    let obj ={};
+    for(let i = 0 ; i < elements.length - 1 ; i++){
+        let item = elements.item(i);
+        obj[item.name] = item.value;
+    }
+
+    const req = new XMLHttpRequest();
+    req.open("POST", "http://localhost:8080/createPost");
+    req.onload = () => {
+        if (req.status === 200 && req.readyState == 4) {
+            console.log("Server Responded with: " + req.responseText);
+        } else {
+            console.log("Oops...");
+        }
+    };
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.send(JSON.stringify({ title: obj.title, body: obj.body, blog:{ id: Number(obj.blogId)} }));
+}
